e0d3dcb52ad2bd8114f395251d4177eee7aea64d
abujayed007 <abujayed007@gmail.com>
2022-10-23 00:32:30 +0600

    server commited

--- index.js
+++ index.js
@@ -5,14 +5,36 @@ const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json')
+
 app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('Node Is Running')
 })
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
 app.get('/news-categories',(req, res) =>{
     res.send(categories)
+})
+
+app.get('/category/:id', (req, res ) =>{
+    const id = req.params.id
+   if ( id === '08'){
+    res.send(news)
+   }
+   else{
+    const categoryId = news.filter( n => n.category_id === id)
+    res.send(categoryId)
+   }
+})
+app.get('/news/:id',(req, res) =>{
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
 })
 
 app.listen(port, () =>{
